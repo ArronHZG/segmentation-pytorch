17500708150eaa42eb2f4fd4e0b17a36d8e3cb3c
ArronHZG <hou.zg@foxmail.com>
2019-08-24 19:24:28 +0800

    rewrite aspp

--- torch_model/net/DeepLabV3Plus/models/aspp.py
+++ torch_model/net/DeepLabV3Plus/models/aspp.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .utils import initialize_weights
+
+# from .utils import initialize_weights
 
 
 def assp_branch(in_channels, out_channles, kernel_size, dilation):
@@ -11,6 +12,20 @@ def assp_branch(in_channels, out_channles, kernel_size, dilation):
         nn.Conv2d(in_channels, out_channles, kernel_size, padding=padding, dilation=dilation, bias=False),
         nn.BatchNorm2d(out_channles),
         nn.ReLU(inplace=True))
+
+
+class ImagePooling(nn.Module):
+    def __init__(self, in_channels):
+        super(ImagePooling, self).__init__()
+
+        self.avg_pool = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(in_channels, 256, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True))
+
+    def forward(self, x):
+        return F.interpolate(self.avg_pool(x), size=(x.size(2), x.size(3)), mode='bilinear', align_corners=True)
 
 
 class ASSP(nn.Module):
@@ -28,26 +43,21 @@ class ASSP(nn.Module):
         self.aspp2 = assp_branch(in_channels, 256, 3, dilation=dilations[1])
         self.aspp3 = assp_branch(in_channels, 256, 3, dilation=dilations[2])
         self.aspp4 = assp_branch(in_channels, 256, 3, dilation=dilations[3])
-
-        self.avg_pool = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(in_channels, 256, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True))
+        self.aspp5 = ImagePooling(in_channels)
 
         self.conv1 = nn.Conv2d(256 * 5, 256, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(256)
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(0.5)
 
-        initialize_weights(self)
+        # initialize_weights(self)
 
     def forward(self, x):
         x1 = self.aspp1(x)
         x2 = self.aspp2(x)
         x3 = self.aspp3(x)
         x4 = self.aspp4(x)
-        x5 = F.interpolate(self.avg_pool(x), size=(x.size(2), x.size(3)), mode='bilinear', align_corners=True)
+        x5 = self.aspp5(x)
 
         x = self.conv1(torch.cat((x1, x2, x3, x4, x5), dim=1))
         x = self.bn1(x)
