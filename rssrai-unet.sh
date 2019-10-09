137cf8285458ea20a0f1259eb90883ad68f275f8
ArronHZG <hou.zg@foxmail.com>
2019-10-09 17:28:57 +0800

    train u-net

--- rssrai-unet.sh
+++ rssrai-unet.sh
@@ -2,7 +2,7 @@ python   train.py  \
         --model UNet \
         --dataset rssrai \
         --gpu-ids 0 \
-        --batch-size=60  \
+        --batch-size=50  \
         --val-batch-size=25 \
         --crop-size=256 \
-        --check-point-id=1
+        --check-point-id=5
