3b5cfa874418415c8a24429e2e26712e8742c4c3
ArronHZG <hou.zg@foxmail.com>
2019-10-14 15:58:53 +0800

    test

---
+++ rssrai-fcn2-test.sh
@@ -0,0 +1,8 @@
+python  test.py  \
+        --model FCN \
+        --dataset rssrai \
+        --gpu-ids 0 \
+        --batch-size=100  \
+        --crop-size=256 \
+        --epochs 3000 \
+        --check-point-id=1
